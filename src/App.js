58252f0924f7531d36be4d8292d9b9084f6692b2
iamleahb <leah.ball@gmail.com>
2022-01-20 23:02:30 -0600

    mvp met

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import data from "./data";
 
 import ProductContext from "./contexts/ProductContexts";
+import CartContext from "./contexts/CartContext";
 
 // Components
 import Navigation from "./components/Navigation";
@@ -19,18 +20,20 @@ function App() {
 
   return (
     <div className="App">
-      <Navigation cart={cart} />
+      <CartContext.Provider value={cart}>
+        <Navigation />
 
-      {/* Routes */}
-      <ProductContext.Provider value={{ products, addItem }}>
-        <Route exact path="/">
-          <Products />
+        {/* Routes */}
+        <ProductContext.Provider value={{ products, addItem }}>
+          <Route exact path="/">
+            <Products />
+          </Route>
+        </ProductContext.Provider>
+
+        <Route path="/cart">
+          <ShoppingCart />
         </Route>
-      </ProductContext.Provider>
-
-      <Route path="/cart">
-        <ShoppingCart cart={cart} />
-      </Route>
+      </CartContext.Provider>
     </div>
   );
 }
